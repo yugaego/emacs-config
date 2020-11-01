159c0b687503992fa7301dcda278ade200220831
Y. E <yugenekr@gmail.com>
2020-11-01 13:46:06 +0200

    Install markdown-mode from Melpa

--- init.el
+++ init.el
@@ -158,3 +158,24 @@
               (whitespace-mode +1))))
 
 
+;; ===================================
+;; Packages
+;; ===================================
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
