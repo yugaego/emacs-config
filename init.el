54b81211ae634bf303ae1392c83aae51ac0bcb66
Y. E <yugenekr@gmail.com>
2020-11-02 15:30:28 +0200

    Separate customize

--- init.el
+++ init.el
@@ -171,28 +171,12 @@
 ;; ===================================
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
-
-;; markdown-mode
-(custom-set-variables
- '(markdown-command "pandoc"))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 
 ;; ===================================
 ;; Custom
 ;; ===================================
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load-file custom-file)
