9d416b235ce7eff5d49b80b3703dc22f68c6c31c
Y. E <yugenekr@gmail.com>
2021-01-21 18:21:57 +0200

    Add alternate key binding for 'other-window

--- inits/init-keybindings.el
+++ inits/init-keybindings.el
@@ -1,5 +1,8 @@
 ;;; init-keybindings.el --- Custom keymap   -*- lexical-binding: t -*-
 
+;; Alternative to C-x o.
+(global-set-key (kbd "C-<tab>") 'other-window)
+
 ;; Alternative to M-d.
 (global-set-key (kbd "M-<delete>") 'kill-word)
 
