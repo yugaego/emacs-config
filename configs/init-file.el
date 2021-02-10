2fddd7cc56297d7f23cb52bc689905c150aa8872
Y. E <yugenekr@gmail.com>
2021-02-10 20:05:13 +0200

    Extract files related configuration

---
+++ configs/init-file.el
@@ -0,0 +1,48 @@
+;;; init-file.el --- Configure files handling   -*- lexical-binding: t -*-
+
+;; Move deleted files to trash.
+(setq delete-by-moving-to-trash t)
+
+;; Fix Mac trashing.
+(when *is-a-mac*
+  ;; To improve performance, ~$ brew install trash~.
+  (require 'osx-trash)
+  (osx-trash-setup))
+
+
+;;; Backups.
+;; Emacs saves one one backup by default,
+;; so we use 'auto-save-hook to force backups.
+(setq make-backup-files t         ; Backup a file the first time it is saved.
+      backup-directory-alist `(("." . ,(concat user-emacs-directory ".backups"))) ; Save to this directory.
+      backup-by-copying t         ; Preserve file owner and group.
+      version-control t           ; Save numbered files.
+      delete-old-versions t       ; Delete old versions silently.
+      kept-new-versions 10        ; Number of newest versions to keep.
+      kept-old-versions 10)       ; Number of oldest versions to keep.
+
+
+;;; Auto-saving.
+;; Configured to be used for it 'auto-save-hook.
+;; To force auto-save at a given moment, use M-x do-auto-save.
+(setq auto-save-default t          ; Turn on auto-saving.
+      auto-save-visited-mode t     ; Auto-save current file-visiting buffer, after 5 sec by default.
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)) ; Save to a temporary directory.
+      auto-save-interval 200       ; Save after N characters typed.
+      auto-save-timeout 600)       ; Save after N seconds of idleness.
+
+
+;;; Force backup on each auto-save.
+;; https://www.emacswiki.org/emacs/ForceBackups
+(defun force-buffer-backup ()
+  (let ((buffer-backed-up nil))
+    (backup-buffer)))
+(add-hook 'after-save-hook 'force-buffer-backup)
+(add-hook 'auto-save-hook 'force-buffer-backup)
+
+
+;;; Built-in vc (version control) package configuration.
+(setq vc-handled-backends '(Git) ; Disable other VC systems to improve performance.
+      vc-make-backup-files t     ; Backup files under VC system.
+      vc-command-messages t)     ; Output shell commands vc package executes.
+
--- init.el
+++ init.el
@@ -4,11 +4,11 @@
 ;; Installed Mac OS packages
 ;; ===================================
 ;;
-;; Pre-installed `$ brew install jansson` for better performance
-;; `$ brew install pandoc` for markdown-mode
-;; `$ brew install aspell` for ispell and flyspell
-;; `$ brew install trash` for osx-trash
-;; `$ brew install mit-scheme` for MIT/GNU Scheme REPL by geiser
+;; Pre-installed `$ brew install jansson` for better performance.
+;; `$ brew install pandoc` for markdown-mode.
+;; `$ brew install aspell` for ispell and flyspell.
+;; `$ brew install trash` to improve performance of osx-trash.
+;; `$ brew install mit-scheme` for MIT/GNU Scheme REPL by geiser.
 
 ;; ===================================
 ;; Backward Compatibility
@@ -25,6 +25,8 @@
 ;; Make more executables available to Emacs.
 (add-to-list 'exec-path "/usr/local/bin")
 
+(defconst *is-a-mac* (eq system-type 'darwin))
+
 ;; Load private settings.
 (let ((private-init (expand-file-name "private-init.el" user-emacs-directory)))
   (when (file-exists-p private-init)
@@ -34,43 +36,12 @@
 ;; If needed, rename a file to disable (enable) its load on Emacs startup.
 (mapc 'load (file-expand-wildcards (concat user-emacs-directory "configs/init-*.el")))
 
-;;; Modes
-(setq auto-save-visited-mode t) ; save file-visiting buffers in 5 seconds
-
 ;;; Session
 (setq-default history-length 1000) ; size of command history
 (savehist-mode t) ; save command history between sessions
 (setq-default default-directory "~/")
 (desktop-save-mode 1) ; save session automatically
 (setq desktop-save t) ; do not ask, always save
-
-;;; Version Control
-(setq
- vc-handled-backends '(Git) ; disables other VC systems to improve performance
- vc-make-backup-files 1 ; backup files under VC system
- vc-command-messages 1) ; output shell commands vc executes
-
-;;; Backups and Auto-saving
-(setq
- backup-directory-alist `(("." . ,(concat user-emacs-directory ".backups")))
- backup-by-copying t ; don't clobber symlinks
- version-control t ; save numbered files
- delete-old-versions t ; delete files silently
- kept-new-versions 10
- kept-old-versions 4)
-(setq
- auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
- auto-save-default t
- auto-save-interval 200 ; characters between auto-saves
- auto-save-timeout 600 ; save after N seconds of idleness
- delete-auto-save-files nil) ; do not delete on buffer saving
-;; Backup on each save
-;; https://www.emacswiki.org/emacs/ForceBackups
-(defun force-buffer-backup ()
-  (let ((buffer-backed-up nil))
-    (backup-buffer)))
-(add-hook 'after-save-hook 'force-buffer-backup)
-(add-hook 'auto-save-hook 'force-buffer-backup)
 
 ;;; Mark region
 (setq highlight-nonselected-windows t)
@@ -85,9 +56,6 @@
  undo-limit 8000000 ; 8 MB
  undo-strong-limit 12000000 ; 12 MB
  undo-outer-limit 20000000) ; 20 MB
-
-;;; Files
-(setq delete-by-moving-to-trash t)
 
 ;;; Dired
 (setq dired-auto-revert-buffer t) ; keep the buffer up-to-date
@@ -108,10 +76,6 @@
 
 (require 'ido)
 (ido-mode t)
-
-(require 'osx-trash)
-(when (eq system-type 'darwin)
-  (osx-trash-setup))
 
 (require 'geiser)
 (setq geiser-active-implementations '(mit))
