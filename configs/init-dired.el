614b947b23789125258c83d7dc11b487f955328d
Y. E <yugenekr@gmail.com>
2021-02-15 15:04:57 +0200

    Experiment key bindings to open favorites in Dired

--- configs/init-dired.el
+++ configs/init-dired.el
@@ -6,3 +6,12 @@
 ;; Auto-refresh Dired when file changes.
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
+;;; Experimental: Key bindings to open favorite directories in Dired.
+(global-set-key (kbd "C-c s-H") (lambda() (interactive) (dired "~/")))
+
+(global-set-key (kbd "C-c s-D") (lambda() (interactive) (dired "~/Documents")))
+
+(global-set-key (kbd "C-c s-P") (lambda() (interactive) (dired "~/Programs")))
+
+(global-set-key (kbd "C-c s-S") (lambda() (interactive) (dired "~/Sites")))
+
