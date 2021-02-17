900c98fdf23a54660f0cac3ae7b578955dd6470a
Y. E <yugenekr@gmail.com>
2021-02-17 16:01:51 +0200

    Remove geiser dependency

--- configs/init-scheme.el
+++
@@ -1,8 +0,0 @@
-;;; init-scheme.el --- Configure Scheme interaction   -*- lexical-binding: t -*-
-
-(require 'geiser)
-
-(setq geiser-active-implementations '(mit))
-
-(setq geiser-repl-query-on-kill-p nil)
-
