30995e2e09c9200e516ceddf0cec21dc46470bdc
Y. E <yugenekr@gmail.com>
2021-02-21 13:48:00 +0200

    Set frames sizes using 0.0 to 1.0 ratio

--- early-init.el
+++ early-init.el
@@ -4,15 +4,22 @@
 ;; Remember to M-x disable-theme after M-x load-theme to get a cleaner UI.
 (load-theme 'misterioso t)
 
+;; Initial frame configuration.
+;; Uses 0.0 to 1.0 ratio.
 (setq initial-frame-alist
-      '((top . 0) (left . 0)
-        (width . 118) (height . 65)
-        (font . "Monaco-15"))) ; or Menlo (close to DejaVu Sans Mono).
+      '((top . 0.0)
+        (left . 0.0)
+        (width . 0.6)
+        (height . 1.0)
+        (font . "Monaco-15")))          ; or Menlo (DejaVu Sans Mono on Linux, Consolas on Windows).
 
+;; New frames configuration.
+;; Uses 0.0 to 1.0 ratio.
 (setq default-frame-alist
-      '((top . 0) (left . 1090)
-        (width . 90) (height . 65)
-        (vertical-scroll-bars)
+      '((top . 0.0)
+        (left . 1.0)
+        (width . 0.4)
+        (height . 1.0)
         (font . "Monaco-15")))
 
 (setq inhibit-startup-message t
