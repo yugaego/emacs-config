558015d244d77044e143c6d9a8405ead478f83ae
Y. E <yugenekr@gmail.com>
2021-03-04 16:01:23 +0200

    Highlight trailing whitespaces

--- configs/init-editing.el
+++ configs/init-editing.el
@@ -41,6 +41,20 @@
 (setq-default tab-width 4)
 
 
+;;; Highlight Trailing Whitespaces
+
+;; Make sure highlighting disabled by default.
+(setq-default show-trailing-whitespace nil)
+
+(defun yet/show-trailing-whitespace ()
+  "Highlight trailing whitespaces in the buffer."
+  (setq-local show-trailing-whitespace t))
+
+;; Highlight trailing whitespaces only in these modes.
+(dolist (hook '(prog-mode-hook text-mode-hook))
+  (add-hook hook 'yet/show-trailing-whitespace))
+
+
 ;;; Duplicate line or region N times.
 ;; https://rejeep.github.io/emacs/elisp/2010/03/11/duplicate-current-line-or-region-in-emacs.html
 (defun yet/duplicate-current-line-or-region (&optional arg)
