9ffd4be16adc1ff46fcdfdb13b5bc48699e516b5
YE <yet@ego.team>
2021-08-15 21:53:38 +0300

    Add directory variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,8 @@
-((emacs-lisp-mode
-  (indent-tabs-mode . nil)))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((tab-width . 4)
+         (indent-tabs-mode . nil)
+         (sentence-end-double-space . nil)
+         (fill-column . 80)))
+ (emacs-lisp-mode . ((emacs-lisp-docstring-fill-column . 75))))
