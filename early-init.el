efc6d65156dc02e435e310ec5fc9f9468f133773
YE <yet@ego.team>
2021-08-16 08:08:08 +0300

    Load OS specific font and increase font size

--- early-init.el
+++ early-init.el
@@ -1,22 +1,27 @@
 ;;; early-init.el --- Loaded before init.el (Emacs 27+)   -*- lexical-binding: t -*-
 
-;; Initial frame configuration.
-;; Uses 0.0 to 1.0 ratio.
-(setq initial-frame-alist
-      '((top . 0.0)
-        (left . 0.0)
-        (width . 0.52)
-        (height . 1.0)
-        (font . "Monaco-15")))          ; or Menlo (DejaVu Sans Mono on Linux, Consolas on Windows).
+(defconst yet/font
+  (cond ((eq system-type 'darwin) "Monaco-17")
+        ((memq system-type '(ms-dos windows-nt)) "Consolas-17")
+        (t "DejaVu Sans Mono-17")))
 
 ;; New frames configuration.
 ;; Uses 0.0 to 1.0 ratio.
 (setq default-frame-alist
-      '((top . 0.0)
+      `((top . 0.0)
         (left . 1.0)
         (width . 0.48)
         (height . 1.0)
-        (font . "Monaco-15")))
+        (font . ,yet/font)))
+
+;; Initial (first) frame configuration.
+;; Uses 0.0 to 1.0 ratio.
+(setq initial-frame-alist
+      `((top . 0.0)
+        (left . 0.0)
+        (width . 0.52)
+        (height . 1.0)
+        (font . ,yet/font)))
 
 (setq inhibit-startup-message t         ; Do not display startup message.
       initial-scratch-message nil)      ; Start with empty *scratch* buffer.
