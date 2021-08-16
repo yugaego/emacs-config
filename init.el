e5c76bbdec2c2381f05be0e7fa3871d4012156ca
YE <yet@ego.team>
2021-08-16 15:54:52 +0300

    Configure exec-path

--- init.el
+++ init.el
@@ -1,15 +1,18 @@
 ;;; init.el --- Main configuration file   -*- lexical-binding: t -*-
 
-;; Make more executables available to Emacs.
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-
 (defconst *is-a-mac* (eq system-type 'darwin))
 
 ;; Pre-load optional local settings.
 (let ((local-pre-init (expand-file-name "local-pre-init.el" user-emacs-directory)))
   (when (file-exists-p local-pre-init)
     (load local-pre-init)))
+
+;; Make OS installed executables available to Emacs.
+;; Add `(defconst yet/exec-path '("/usr/local/bin" "/any/other/path"))'
+;; to `local-pre-init.el' file.
+(when (boundp 'yet/exec-path)
+  (dolist (path yet/exec-path)
+    (add-to-list 'exec-path path)))
 
 ;; Load additional configuration files matching the name pattern.
 ;; If needed, rename a file to disable (enable) its load on Emacs startup.
