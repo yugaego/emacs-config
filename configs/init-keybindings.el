77660fadf072d5eb38a23c7b3b1f7d434ca4ed57
YE <yet@ego.team>
2021-08-21 09:18:24 +0300

    Add key binding to toggle scroll-lock-mode

--- configs/init-keybindings.el
+++ configs/init-keybindings.el
@@ -13,3 +13,7 @@
 (global-set-key (kbd "C-z") ctl-x-map) ; alternative to C-x prefix key.
 (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
+;; Toggle scroll-lock-mode.
+;; `l' mnemonic: Lock Line and scroLL.
+(global-set-key (kbd "C-c l") #'scroll-lock-mode)
+
