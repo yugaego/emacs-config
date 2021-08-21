7857f53386d66f18bf4a95868768f9013de41b21
YE <yet@ego.team>
2021-08-21 08:53:39 +0300

    Improve and describe org indentation

--- configs/init-org.el
+++ configs/init-org.el
@@ -1,6 +1,22 @@
-;;; init-org.el --- Configure Org Mode for plain text markup   -*- lexical-binding: t -*-
+;;; init-org.el --- Org: mode for text markup and planning   -*- lexical-binding: t -*-
 
-(setq org-startup-truncated nil         ; Show lines in full.
-      org-startup-folded nil ; Open org files with the items expanded.
-      org-support-shift-select 'always) ; Force enable shift-selection.
+(require 'org)
+
+(setq org-startup-truncated nil        ; Show lines in full.
+      org-startup-folded nil           ; Open org files with the items expanded.
+      org-support-shift-select 'always); Force enable shift-selection.
+
+
+;;; Indentation
+
+;; Do not indent after headlines.
+(setq org-adapt-indentation nil)
+
+;; With electric indent enabled, use in the lists:
+;; - `C-j' to start a new line.
+;; - `RET' to add a child item.
+;; - `M-RET' to add a sibling.
+
+;; Disable electric indent.
+;; (add-hook 'org-mode-hook (lambda () (electric-indent-local-mode -1)))
 
