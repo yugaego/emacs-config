49f2635ea306cff48ca42c706ce52bf97c50d057
YE <yet@ego.team>
2021-09-03 12:38:08 +0300

    Adjust Gnus UI settings

--- configs/init-gnus.el
+++ configs/init-gnus.el
@@ -15,12 +15,14 @@
   ;;; Groups
   (setq gnus-group-buffer "*Gnus Groups*"
         gnus-use-scoring nil
-        gnus-group-line-format "%M%S%p%P%5y:%B%(%-16,16c%) %9,9~(cut 4)d\n"
         gnus-interactive-catchup nil      ; Catchup groups automatically.
         gnus-summary-next-group-on-exit nil)
 
   ;;; Articles
   (setq gnus-auto-select-next nil
         ;; gnus-add-to-list t
-        gnus-summary-line-format "%U%R%z%I%[: %-8,8f %] %s\n"))
+        gnus-summary-line-format "%U%R%z%I%[: %-16,16f %] %s\n")
+
+  (gnus-add-configuration
+   '(article (vertical 1.0 (summary .35 point) (article 1.0)))))
 
