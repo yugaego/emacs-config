7653f8549308bbec76214094a5a4d8411039fc96
YE <yet@ego.team>
2021-09-16 11:24:24 +0300

    Restrict theme adjustments to many-colors displays

--- configs/init-theme.el
+++ configs/init-theme.el
@@ -5,29 +5,28 @@
 ;; do `M-x disable-theme <old-theme>'.
 (load-theme 'misterioso t)
 
-(custom-theme-set-faces
- 'misterioso
- ;; Optional colors for misterioso theme:
- ;; LightBlue4, LightSkyBlue4, LightSteelBlue4,
- ;; SlateGray4, LightSlateGray
- '(cursor ((t (:background "SteelBlue3"))) t)
- '(header-line ((t (:background "#3a4a5a"))) t)
- '(mode-line ((t (:background "SlateGray"))) t)
- '(mode-line-buffer-id ((t (:background "SkyBlue4"))) t)
- '(mode-line-emphasis ((t (:foreground "green4"))) t)
- '(mode-line-highlight ((t (:background "SteelBlue4"))) t)
- '(mode-line-inactive ((t (:background "#212933"))) t)
- '(diff-added ((t (:background "#334433"))) t)
- '(diff-removed ((t (:background "#443333"))) t)
- '(diff-refine-added ((t (:background "#336633"))) t)
- '(diff-refine-removed ((t (:background "#663333"))) t)
- '(show-paren-match ((t (:background "SkyBlue4"))) t)
- '(line-number-current-line ((t (:background "#405060"))) t))
+(let ((class '((class color) (min-colors 4096))))
 
-(when (version<= "28.0.50" emacs-version)
   (custom-theme-set-faces
    'misterioso
-   '(bookmark-face ((t (:background "#2d3743" :foreground "#ffad29"))))))
+   `(cursor ((,class (:background "SteelBlue3"))) t)
+   `(header-line ((,class (:background "#3a4a5a"))) t)
+   `(mode-line ((,class (:background "SlateGray"))) t)
+   `(mode-line-buffer-id ((,class (:background "SkyBlue4"))) t)
+   `(mode-line-emphasis ((,class (:foreground "green4"))) t)
+   `(mode-line-highlight ((,class (:background "SteelBlue4"))) t)
+   `(mode-line-inactive ((,class (:background "#212933"))) t)
+   `(diff-added ((,class (:background "#334433"))) t)
+   `(diff-removed ((,class (:background "#443333"))) t)
+   `(diff-refine-added ((,class (:background "#336633"))) t)
+   `(diff-refine-removed ((,class (:background "#663333"))) t)
+   `(show-paren-match ((,class (:background "SkyBlue4"))) t)
+   `(line-number-current-line ((,class (:background "#405060"))) t))
+
+  (when (version<= "28.0.50" emacs-version)
+    (custom-theme-set-faces
+     'misterioso
+     `(bookmark-face ((,class (:background "#2d3743" :foreground "#ffad29")))))))
 
 (enable-theme 'misterioso)
 
