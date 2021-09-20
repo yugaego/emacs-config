314ab8dd5533b569186922acbd3ca2a9a67a0c1e
YE <yet@ego.team>
2021-09-20 14:21:22 +0300

    Add ELisp mode configurations

---
+++ configs/init-elisp.el
@@ -0,0 +1,38 @@
+;;; init-elisp.el --- Emacs Lisp mode   -*- lexical-binding: t -*-
+
+(require 'flymake)
+
+(defun yet-emacs-lisp-mode ()
+  (setq-local emacs-lisp-docstring-fill-column 75)
+
+  (flymake-mode 1)
+
+  (push '(face . nil) (get :note 'flymake-overlay-control))
+
+  (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+
+  (define-key flymake-mode-map (kbd "M-n")
+    (lambda () (interactive)
+      (let ((current-prefix-arg 1))
+        (call-interactively #'flymake-goto-next-error))))
+
+  (define-key flymake-mode-map (kbd "M-p")
+    (lambda () (interactive)
+      (let ((current-prefix-arg 1))
+        (call-interactively #'flymake-goto-prev-error)))))
+
+(add-hook 'emacs-lisp-mode-hook #'yet-emacs-lisp-mode)
+
+
+(require 'rainbow-mode)
+
+(with-eval-after-load 'rainbow
+
+  (defun yet-emacs-lisp-rainbow ()
+    (let ((buffer-name (buffer-file-name)))
+      (if (and buffer-name
+               (string-match-p "-theme.el" buffer-name))
+      (rainbow-mode 1))))
+
+  (add-hook 'emacs-lisp-mode-hook #'yet-emacs-lisp-rainbow))
+
