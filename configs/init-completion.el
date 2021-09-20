a714ee6c8184a3526b02c8b8dcb3d96f8926c49e
YE <yet@ego.team>
2021-09-20 14:23:52 +0300

    Enable Company mode

--- configs/init-completion.el
+++ configs/init-completion.el
@@ -4,3 +4,60 @@
 
 (add-to-list 'completion-styles 'initials t) ; Suggest completions by initials too.
 
+
+(require 'company)
+
+;; https://github.com/company-mode/company-mode/discussions/1214
+(with-eval-after-load 'company
+
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0
+        company-tooltip-idle-delay 10        ; Enable tooltip manually
+        company-frontends
+        '(company-pseudo-tooltip-unless-just-one-frontend-with-delay
+          company-preview-frontend
+          company-echo-metadata-frontend)
+        company-backends '((company-capf company-dabbrev-code)
+                           company-dabbrev)
+        company-transformers '(delete-consecutive-dups
+                               company-sort-by-occurrence
+                               company-sort-prefer-same-case-prefix)
+        company-format-margin-function nil
+        company-require-match nil
+        company-show-quick-access 'left
+        company-tooltip-align-annotations t
+        company-dabbrev-downcase nil
+        company-dabbrev-other-buffers t
+        company-dabbrev-minimum-length 2
+        company-dabbrev-code-modes t
+        company-dabbrev-code-everywhere t)
+
+  (define-key company-active-map (kbd "M-n") #'company-select-next)
+  (define-key company-active-map (kbd "M-p") #'company-select-previous)
+  (define-key company-active-map (kbd "C-n") nil)
+  (define-key company-active-map (kbd "C-p") nil)
+
+  (global-set-key (kbd "C-c c")
+                  (lambda ()
+                    (interactive)
+                    (let ((company-tooltip-idle-delay 0.0))
+                      ;; Workaround to lookup completions from inside a word.
+                      (if (looking-at "\\S-")
+                          (save-excursion (insert " ")))
+                      (company-complete)
+                      (and company-candidates
+                           (company-call-frontends 'post-command)))))
+
+
+  (defun yet-prog-mode-company ()
+    (company-mode 1))
+
+  (add-hook 'prog-mode-hook #'yet-prog-mode-company)
+
+
+  (defun yet-c-mode-company ()
+    (setq-local company-backends
+                '((company-clang company-dabbrev-code) company-dabbrev)))
+
+  (add-hook 'c-mode-common-hook #'yet-c-mode-company))
+
