243d994644ed8e0fb17105d12fb2d7497a243c73
YE <yet@ego.team>
2021-09-20 17:14:45 +0300

    Improve Dired settings

--- configs/init-dired.el
+++ configs/init-dired.el
@@ -1,8 +1,14 @@
-;;; init-dired.el --- Configure Dired, the Directory Editor   -*- lexical-binding: t -*-
+;;; init-dired.el --- Dired: the Directory Editor   -*- lexical-binding: t -*-
+
+(require 'dired)
 
 (setq dired-auto-revert-buffer t        ; Keep Dired buffers up-to-date.
       dired-listing-switches "-alh")    ; Output human-readable sizes.
 
 ;; Auto-refresh Dired when file changes.
 (add-hook 'dired-mode-hook 'auto-revert-mode)
+
+(when (version<= "28.0.50" emacs-version)
+  (setq dired-kill-when-opening-new-dired-buffer t ; No more buffers spawned!
+        dired-switches-in-mode-line 'as-is))       ; Show all switches.
 
