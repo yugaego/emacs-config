deefac817a49f4d30e421b5cbdf77376da2ddc12
YE <yet@ego.team>
2021-09-20 10:36:40 +0300

    Teach C-a to respect indentation

--- configs/init-editing.el
+++ configs/init-editing.el
@@ -109,3 +109,32 @@ there's a region, all lines that region covers will be duplicated."
       undo-strong-limit 12000000        ; 12 MB
       undo-outer-limit 20000000)        ; 20 MB
 
+
+;;; Teach `C-a' to respect indentation.
+;;; https://prelude.emacsredux.com/en/latest
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key [remap move-beginning-of-line]
+                'prelude-move-beginning-of-line)
+
