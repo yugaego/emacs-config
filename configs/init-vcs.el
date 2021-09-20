6fe5ceb5557b64229d985cbef51635a3d4498cfe
YE <yet@ego.team>
2021-09-20 10:23:52 +0300

    Add diff-hl package to mark git changed hunks

--- configs/init-vcs.el
+++ configs/init-vcs.el
@@ -5,3 +5,16 @@
 (setq vc-handled-backends '(Git) ; Disable other VCS to improve performance.
       vc-make-backup-files t)    ; Still backup files under VCS.
 
+(require 'diff-hl)
+
+(with-eval-after-load 'diff-hl
+  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+  (add-hook 'prog-mode-hook 'diff-hl-flydiff-mode)
+  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote)
+
+  (define-key diff-hl-command-map (kbd "/") #'diff-hl-revert-hunk)
+  (define-key diff-hl-command-map (kbd "/") #'diff-hl-show-hunk-revert-hunk)
+  (define-key diff-hl-command-map (kbd "p") #'diff-hl-show-hunk-previous)
+  (define-key diff-hl-command-map (kbd "n") #'diff-hl-show-hunk-next))
+
