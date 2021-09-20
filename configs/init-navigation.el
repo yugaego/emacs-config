5f73e6140a1183d5aac092d6fa9a802eb64146ad
YE <yet@ego.team>
2021-09-20 14:55:47 +0300

    Require bookmark package

--- configs/init-navigation.el
+++ configs/init-navigation.el
@@ -1,5 +1,7 @@
 ;;; init-navigation.el --- Move around   -*- lexical-binding: t -*-
 
+(require 'bookmark)
+
 (if (boundp 'yet-bookmark-default-file)
     (setq bookmark-default-file yet-bookmark-default-file))
 
