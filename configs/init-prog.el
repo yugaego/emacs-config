51e70c9858dc72200a7d5ce6570815a22c251764
YE <yet@ego.team>
2021-09-26 14:14:03 +0300

    Enable tabs indentation in makefile-mode

--- configs/init-prog.el
+++ configs/init-prog.el
@@ -1,5 +1,13 @@
 ;;; init-prog.el --- Programming languages   -*- lexical-binding: t -*-
 
+;;; Makefile
+
+(defun yet-makefile-mode-hook ()
+  (setq-local indent-tabs-mode t))
+
+(add-hook 'makefile-mode-hook #'yet-makefile-mode-hook)
+
+
 ;;; Completion
 
 (with-eval-after-load 'company
