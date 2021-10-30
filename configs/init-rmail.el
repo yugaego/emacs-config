87ca4bcf1dba86df9b5b05107131d72da2619012
YE <yet@ego.team>
2021-10-30 13:42:43 +0300

    Enable auto-fill in message-mode

--- configs/init-rmail.el
+++ configs/init-rmail.el
@@ -36,7 +36,7 @@
 (defun yet-message-mode ()
   (when (version<= "28.0.50" emacs-version)
     (display-fill-column-indicator-mode 1))
-  (auto-fill-mode -1))
+  (auto-fill-mode 1))
 
 (add-hook 'message-mode-hook #'yet-message-mode)
 
