b463b1c014053c6808966f2b5e0d97294ae0debc
YE <yet@ego.team>
2021-11-01 12:26:09 +0200

    Stop using diff-hl revert custom bindings

--- configs/init-vcs.el
+++ configs/init-vcs.el
@@ -33,8 +33,6 @@
   (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
   (add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote)
 
-  (define-key diff-hl-command-map (kbd "/") #'diff-hl-revert-hunk)
-  (define-key diff-hl-command-map (kbd "/") #'diff-hl-show-hunk-revert-hunk)
   (define-key diff-hl-command-map (kbd "p") #'diff-hl-show-hunk-previous)
   (define-key diff-hl-command-map (kbd "n") #'diff-hl-show-hunk-next)
 
