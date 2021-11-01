3ef3c989395ca333511451ba7aa7b2a126b9540e
YE <yet@ego.team>
2021-11-01 12:33:19 +0200

    Revert to diff-hl custom revert hunk binding

--- configs/init-vcs.el
+++ configs/init-vcs.el
@@ -33,6 +33,7 @@
   (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
   (add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote)
 
+  (define-key diff-hl-command-map (kbd "/") #'diff-hl-revert-hunk)
   (define-key diff-hl-command-map (kbd "p") #'diff-hl-show-hunk-previous)
   (define-key diff-hl-command-map (kbd "n") #'diff-hl-show-hunk-next)
 
