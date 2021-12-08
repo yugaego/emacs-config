620a071d31e3a2319ffce1b8d238a6cd6a9bd95d
Yuga Ego <yet@ego.team>
2021-12-08 20:43:32 +0200

    Fix pdf-tools initialization

--- configs/init-pdf.el
+++ configs/init-pdf.el
@@ -4,21 +4,19 @@
 
 (when (and (boundp 'yet-enable-pdf-tools) yet-enable-pdf-tools)
 
-  (with-eval-after-load 'pdf-tools
+  (pdf-loader-install)
 
-    (pdf-loader-install)
+  (setq-default pdf-view-display-size 'fit-width
+                ;; pdf-view-max-image-width 4800
+                pdf-view-resize-factor 1.05
+                pdf-view-bounding-box-margin 0
+                ;; Fixes contrast on Mac OS Retina
+                ;; by displaying 2x images.
+                pdf-view-use-scaling t
+                pdf-view-use-unicode-ligther nil)
 
-    (setq-default pdf-view-display-size 'fit-width
-                  ;; pdf-view-max-image-width 4800
-                  pdf-view-resize-factor 1.05
-                  pdf-view-bounding-box-margin 0
-                  ;; Fixes contrast on Mac OS Retina
-                  ;; by displaying 2x images.
-                  pdf-view-use-scaling t
-                  pdf-view-use-unicode-ligther nil)
+  (defun yet-pdf-view-mode ()
+    (pdf-view-midnight-minor-mode 1))
 
-    (defun yet-pdf-view-mode ()
-      (pdf-view-midnight-minor-mode 1))
-
-    (add-hook 'pdf-view-mode-hook #'yet-pdf-view-mode)))
+  (add-hook 'pdf-view-mode-hook #'yet-pdf-view-mode))
 
