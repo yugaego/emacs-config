14cc61e951d880ae2d0fab47f893754e901029e2
Yuga Ego <yet@ego.team>
2022-04-30 21:22:40 +0300

    Improve rust-mode usability

--- configs/init-rust.el
+++ configs/init-rust.el
@@ -1,8 +1,11 @@
 ;;; init-rust.el --- Configure Rust Mode   -*- lexical-binding: t -*-
 
+(require 'rust-mode)
+
 (with-eval-after-load 'rust-mode
 
-  (setq rust-format-on-save t)
+  (setq rust-format-on-save nil
+        rust-rustfmt-switches '("--edition" "2021"))
 
   ;; C-c C-f defined by rust-mode runs 'rust-format-buffer.
   (define-key rust-mode-map (kbd "C-c C-c") 'rust-check)
