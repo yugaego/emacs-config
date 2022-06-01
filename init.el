fba128cd42c8f1a6f8418802c9eb51688914962c
Yuga Ego <yet@ego.team>
2022-06-01 17:14:32 +0300

    Experiment with Emacs performance

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;; init.el --- Main configuration file   -*- lexical-binding: t -*-
 
+;; Tweak performance.
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq read-process-output-max (* 1024 1024 2) ; try 1-3 Mb.
+      gc-cons-threshold (* 2 800000)          ; multiply initial 800 Kb by 2.
+      garbage-collection-messages t)
+
+
 ;; Pre-load optional local settings.
 (let ((local-pre-init (expand-file-name "local-pre-init.el" user-emacs-directory)))
   (when (file-exists-p local-pre-init)
