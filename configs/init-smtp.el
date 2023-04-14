5caa8ae01f7a6e880af7f4ac0d34d9281bdc2961
YugaEgo <yet@ego.team>
2023-04-14 21:49:30 +0300

    Improve email sending setup description

--- configs/init-smtp.el
+++ configs/init-smtp.el
@@ -5,29 +5,42 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; *Basic*
-;; 1. $ cp ~/.emacs.d/example/local-pre-init.el ~/.emacs.d/local-pre-init.el
-;; 2. Fill in 'init-smtp' section in '~/.emacs.d/local-pre-init.el'
-;; 3. Restart or `M-x eval-buffer' on local-pre-init.el and this file.
-;; 3. Call `C-x m' (`M-x compose-mail'), fill it, then `C-c C-c' to send it.
-;; 4. Enter password when asked and choose to save '~/.authinfo'.
+;; - $ cp ~/.emacs.d/example/local-pre-init.el ~/.emacs.d/local-pre-init.el
+;; - Fill in 'init-smtp' section in '~/.emacs.d/local-pre-init.el'
+;; - Restart or `M-x eval-buffer' on local-pre-init.el and this file.
+;; - Call `C-x m' (`M-x compose-mail'), fill it, then `C-c C-c' to send it.
+;; - Enter password when asked and choose to save '~/.authinfo'.
 ;; The following line will be written to the file:
 ;; machine smtp.domain.tld login user@domain.tld password *** port 465
+;; - For more details, consult (info "(auth) Help for users").
 ;;
-;;; Troubleshooting
+;;; Troubleshoot
 ;; Error: "movemail: could not create mailbox ... Requested item not found"
 ;; Make sure 'mailutils' was compiled with support for TLS.
 ;;
 ;;; *Secure*
-;; 5. Encrypt ~/.authinfo as ~/.authinfo.gpg
-;;    - For example, in Dired encrypt ~/.authinfo by pressing :e
+;; - Encrypt ~/.authinfo as ~/.authinfo.gpg
+;;   - In 'dired-mode': go to '~/.authinfo' and type `:e' (`epa-dired-do-encrypt').
+;; - Remove '~/.authinfo'.
 ;;
-;;; Troubleshooting
+;;; Troubleshoot
 ;; Error: "gpg problem with the agent: Inappropriate ioctl for device:".
 ;; Fix: https://d.sb/B5N.
 ;; 1. $ printf "use-agent\npinentry-mode loopback\n" >> ~/.gnupg/gpg.conf
 ;; 2. $ printf "allow-loopback-pinentry\n" >> ~/.gnupg/gpg-agent.conf
 ;; 3. Restart the agent:
 ;;    - $ echo RELOADAGENT | gpg-connect-agent
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Email addresses management ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Use packages 'Mailabbrev' or 'BBDB'.
+;; 'Mailabbrev' is straightforward: create `~/.mailrc' with, f.i.:
+;; alias me "My Name <my@example.com>"
+;; alias others "Another Name <they@example.com>" "Other <she@example.com>"
+;; Then hit `RET' or `TAB' to complete "me" or "others" in 'message-mode'.
+;; 'BBDB' is more sophisticated and can be configured to auto-collect addresses.
 
 (require 'smtpmail)
 (require 'message)
