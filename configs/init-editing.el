37da2adcfc5b949de0a7fd32c7c3d6d3b51328b8
YugaEgo <yet@ego.team>
2023-09-03 19:55:02 +0300

    Add hash sign to the built-in function name

--- configs/init-editing.el
+++ configs/init-editing.el
@@ -27,7 +27,7 @@
 ;; Auto-insert closing parenthesis.
 (require 'elec-pair)
 (electric-pair-mode 1)
-(setq electric-pair-skip-self 'electric-pair-default-skip-self)
+(setq electric-pair-skip-self #'electric-pair-default-skip-self)
 
 ;; How many times cursor blinks.
 ;; <1 means blink forever.
