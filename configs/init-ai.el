d7a2c8a4b01ca71580f04f79fbd8976f5fe1e909
YugaEgo <yet@ego.team>
2024-01-13 18:41:52 +0200

    Minor adjustments

--- configs/init-ai.el
+++ configs/init-ai.el
@@ -1,10 +1,12 @@
-;;; init-ai.el --- AI chatbot  -*- lexical-binding: t -*-
+;;; init-ai.el --- LLM (AI) related configurations   -*- lexical-binding: t -*-
+
+(require 'gptel)
 
 (with-eval-after-load 'gptel
-  (setopt gptel-default-mode 'org-mode
-          gptel-max-tokens 500
-          gptel-directives
-          '((default . "You are a helpful assistant. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer.")
-            (programming . "You are a helpful assistant and careful programmer. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer. When only code is asked, provide only code, without any comments and notes.")
-            (writing . "You are a helpful assistant for editing, proofreading, writing, and translating texts. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer.")
-            (chat . "You are a conversation partner. Provide clear and thorough answers but be concise."))))
+  (setq-default gptel-default-mode 'org-mode
+                gptel-max-tokens 500
+                gptel-directives
+                '((default . "You are a helpful assistant. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer.")
+                  (programming . "You are a helpful assistant and careful programmer. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer. When only code is asked, provide only code, without any comments and notes.")
+                  (writing . "You are a helpful assistant for editing, proofreading, writing, and translating texts. Provide clear and thorough answers but be concise. Let's work this out in a step by step way to be sure we have the right answer.")
+                  (chat . "You are a conversation partner. Provide clear and thorough answers but be concise."))))
