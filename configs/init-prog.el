e008bb56f342d60138b21d8036b99fbe46ce43c9
YugaEgo <yet@ego.team>
2025-06-30 18:34:29 +0300

    Extract 'configs/init-treesit.el' file

--- configs/init-prog.el
+++ configs/init-prog.el
@@ -1,4 +1,4 @@
-;;; init-prog.el --- Programming languages   -*- lexical-binding: t -*-
+;;; init-prog.el --- Programming languages  -*- lexical-binding: t -*-
 
 ;;; Flymake: syntax checker
 
@@ -144,16 +144,3 @@ For more details, see command `hs-hide-all'."
   (toggle-truncate-lines 1))
 
 (add-hook 'sql-interactive-mode-hook 'yet-sql-interactive-mode)
-
-;;; Dockerfile
-
-;; A temporary workaround to provide a recipe for the Dockerfile grammar
-;; installation with the function `treesit-install-language-grammar'.
-;; By the next Emacs release (30.2 or 31.1) the variable
-;; `treesit-language-source-alist' is expected to include this recipe.
-;; See commit 3d3be6dd0eb320e73d59ad6958e7c6e2c1b6a434 in Emacs repo.
-(when (and (boundp 'treesit-language-source-alist)
-           (not (assoc 'dockerfile treesit-language-source-alist)))
-  (add-to-list
-   'treesit-language-source-alist
-   '(dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")))
