6f98e1d5a0f3d0ef6e7644eb42bbd63afe9d0a2b
YugaEgo <yet@ego.team>
2025-06-30 18:27:15 +0300

    Do not hide comments

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,7 @@
 ((nil . ((tab-width . 4)
          (indent-tabs-mode . nil)
          (sentence-end-double-space . nil)
-         (fill-column . 80)))
+         (fill-column . 80)
+         (eval . (hs-minor-mode 0))))
  (org-mode . ((tab-width . 8)))
  (emacs-lisp-mode . ((emacs-lisp-docstring-fill-column . 75))))
