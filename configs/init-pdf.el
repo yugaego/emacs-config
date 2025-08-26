fd0e11de0261baf9a816e84fa1cc0b4988f7ebd6
YugaEgo <yet@ego.team>
2025-08-26 09:54:02 +0300

    Enable 'pdf-view-midnight-minor-mode' only for dark theme

--- configs/init-pdf.el
+++ configs/init-pdf.el
@@ -11,9 +11,19 @@
 
     (pdf-loader-install)
 
+    (defun yet-enabled-theme-dark-p ()
+      "Return non-nil if the current theme has a dark background."
+      (let* ((theme (car custom-enabled-themes))
+             (bg (face-background 'default nil theme)))
+        (when (stringp bg)
+          (let* ((rgb (color-name-to-rgb bg))
+                 (lum (apply #'+ (mapcar (lambda (c) (* c 0.3)) rgb))))
+            (< lum 0.5)))))
+
     (defun yet-pdf-view-mode ()
       (setq-local display-line-numbers nil)
-      (pdf-view-midnight-minor-mode 1)
+      (when (yet-enabled-theme-dark-p)
+        (pdf-view-midnight-minor-mode 1))
       (setq-default pdf-view-display-size 'fit-height
                     ;; pdf-view-max-image-width 4800
                     pdf-view-resize-factor 1.05
