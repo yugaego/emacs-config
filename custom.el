c892a90db8c56f788335e5f2d477404a1c1a74d8
YugaEgo <yet@ego.team>
2026-03-14 10:53:34 +0200

    Add package nix-mode

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(auto-dark company debbugs diff-hl eglot exec-path-from-shell fish-mode gptel
-               htmlize imenu-list markdown-mode org-contrib pdf-tools php-mode
-               racket-mode rainbow-mode rust-mode transient vterm web-mode)))
+               htmlize imenu-list markdown-mode nix-mode org-contrib pdf-tools
+               php-mode racket-mode rainbow-mode rust-mode transient vterm
+               web-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
